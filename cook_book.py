a83fe9dd825242dcdcf31e035efaef19e5587637
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-18 22:33:55 +0200

    Реализовано формирование словарей по каждому блюду. Нерешено: удаление или игнорирование пустых строк; объединение в один словарь

--- cook_book.py
+++ cook_book.py
@@ -3,16 +3,40 @@
 # Стр-ра построчная:
 # 1 - название блюда, 2 - кол-во ингредиентов
 # 3 и далее - ингредиенты (name | quantity | measure)
+# Сгенерировать список
 
+cook_book = {}
+dish = {}
 
 with open('cook_book.txt') as book:
     for line in book:
-        type(line)
         dish_name = line.strip()
-        quantity_ingredients = int(book.readline())
-        print(dish_name, quantity_ingredients)
-        for ingridient in range(quantity_ingredients + 1):
-            ingridient = book.readline().strip()
-            print(ingridient)
+        quantity_ingridients = int(book.readline())
+
+        dish['name'] = dish_name
+        dish['type'] = '2'
+#        print(dish)
+
+        list_of_ingridients = []
+
+        for ingridient in range(quantity_ingridients):
+
+            dict_ingridients = {}
+            ingridient = list(book.readline().strip().split('|'))
+#            print('характеристика ингридиента', ingridient)
+            product = ingridient[0]
+            quantity = ingridient[1]
+            unit = ingridient[2]
+
+            dict_ingridients['unit'] = unit
+            dict_ingridients['quantity'] = quantity
+            dict_ingridients['product'] = product
+            list_of_ingridients.append(dict_ingridients)
+#            print(ingridient)
+#            print(dict_ingridients)
+#            print('List of ingridients', list_of_ingridients)
+        dish['ingridients'] = list_of_ingridients
+        cook_book[dish_name] = dish
+        print(cook_book)
 
 #        print (line.strip())
