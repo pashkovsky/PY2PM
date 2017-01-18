e7421237c74e112ba9239cb8d4039b609263cb09
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-18 23:10:29 +0200

    v0.1 Реализовано формирование словарей по каждому блюду в общем словаре

--- cook_book.py
+++ cook_book.py
@@ -9,34 +9,35 @@ cook_book = {}
 dish = {}
 
 with open('cook_book.txt') as book:
+
     for line in book:
-        dish_name = line.strip()
-        quantity_ingridients = int(book.readline())
+        if not line.isspace():
+            dish_name = line.strip()
+            quantity_ingridients = int(book.readline())
+            dish['name'] = dish_name
+            dish['type'] = '2'
+    #        print(dish)
 
-        dish['name'] = dish_name
-        dish['type'] = '2'
-#        print(dish)
+            list_of_ingridients = []
 
-        list_of_ingridients = []
+            for ingridient in range(quantity_ingridients):
 
-        for ingridient in range(quantity_ingridients):
+                dict_ingridients = {}
+                ingridient = list(book.readline().strip().split('|'))
+    #            print('характеристика ингридиента', ingridient)
+                product = ingridient[0]
+                quantity = ingridient[1]
+                unit = ingridient[2]
 
-            dict_ingridients = {}
-            ingridient = list(book.readline().strip().split('|'))
-#            print('характеристика ингридиента', ingridient)
-            product = ingridient[0]
-            quantity = ingridient[1]
-            unit = ingridient[2]
-
-            dict_ingridients['unit'] = unit
-            dict_ingridients['quantity'] = quantity
-            dict_ingridients['product'] = product
-            list_of_ingridients.append(dict_ingridients)
-#            print(ingridient)
-#            print(dict_ingridients)
-#            print('List of ingridients', list_of_ingridients)
-        dish['ingridients'] = list_of_ingridients
-        cook_book[dish_name] = dish
-        print(cook_book)
+                dict_ingridients['unit'] = unit
+                dict_ingridients['quantity'] = quantity
+                dict_ingridients['product'] = product
+                list_of_ingridients.append(dict_ingridients)
+    #            print(ingridient)
+    #            print(dict_ingridients)
+    #            print('List of ingridients', list_of_ingridients)
+            dish['ingridients'] = list_of_ingridients
+            cook_book[dish_name] = dish
+    print(cook_book)
 
 #        print (line.strip())
