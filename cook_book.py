23ae5bb517020506402cae3b6da016344d46b3cd
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-18 14:48:50 +0200

    Функционирует построчный разбор текста файла cook_book.txt

--- cook_book.py
+++ cook_book.py
@@ -1,3 +1,18 @@
+# Генеральная задача - сформировать словарь в рез-те
+# чтения списка рецептов из файла cook_book.txt
+# Стр-ра построчная:
+# 1 - название блюда, 2 - кол-во ингредиентов
+# 3 и далее - ингредиенты (name | quantity | measure)
+
+
 with open('cook_book.txt') as book:
     for line in book:
-        print (line.strip())
+        type(line)
+        dish_name = line.strip()
+        quantity_ingredients = int(book.readline())
+        print(dish_name, quantity_ingredients)
+        for ingridient in range(quantity_ingredients + 1):
+            ingridient = book.readline().strip()
+            print(ingridient)
+
+#        print (line.strip())
