f34d4f9f54d660a84e57f6bcc0d8d8ff813d1db0
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-23 23:24:53 +0200

    Разборка json. Ошибка в строке 14 на item

--- PY2_3/findout_words.py
+++ PY2_3/findout_words.py
@@ -7,9 +7,19 @@ def parsed_string():
     with codecs.open('newsfr.json', encoding="iso8859_5") as news:
         return json.load(news)
 
-for word in parsed_string():
-    if len(word) > 6:
-        print(word)
+#print (parsed_string())
+
+for rss in parsed_string().values():
+    for channel in rss.values():
+        for item in channel['item']:
+            for description in item['description'].values():
+#                print(description)
+                for word in description:
+                    print (word)
+
+# for word in parsed_string():
+#     if len(word) > 6:
+#         print(word)
 
 
     # for channel in news.values():
