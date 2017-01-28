6592d777686d0ff6f84c429108e6f206e90c27e3
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-28 15:20:49 +0200

    Код find_procedure.py читает файлы из двух папок

--- PY2_4/find_procedure.py
+++ PY2_4/find_procedure.py
@@ -3,13 +3,18 @@ import os
 
 
 migrations = 'Migrations'
+advanced_migrations = 'Advanced Migrations'
 
 # Отбираем файлы по маске
 files = glob.glob(os.path.join(migrations, "*.sql"))
+files2 = glob.glob(os.path.join(advanced_migrations, "*.sql"))
 
 # Cписок файлов отобранных по маске
 files_list =list(files)
-print('Начальное количество файлов', len(files_list)) # Убрать
+for file in files2:
+    files_list.append(file)
+
+print('Количество файлов, которые проверяются в запросе: ', len(files_list))
 
 # Контрольный список файлов, которые не содержат строку поиска
 files_str_out = []
@@ -18,11 +23,18 @@ files_str_out = []
 def find_str(files_list, files_str_out, search_line):
     for file_name in files_list:
         with open(file_name) as file:
-            data = file.read()
-            row = data.find(search_line)
-            if  row == -1:
-                files_str_out.append(file_name)
-                files_list.remove(file_name)
+            try:
+                data = file.read()
+                row = data.find(search_line)
+                if  row == -1:
+                    files_str_out.append(file_name)
+                    files_list.remove(file_name)
+            except:
+                print('Обшибка в кодировке файла {0}'.format(file_name))
+
+    return files_list
+
+# Часть кода, если нужна информация на какой строке файла выполняется впервые поисковый запрос
 #			print('Файл {0} удален'.format(file_name)) # Закомментировать. Проверка файлов по списку
 #		else:
 #			print('Слова {0} встречаются впервые в файле\
@@ -40,7 +52,7 @@ def list_files_str_out(files_str_out):
         print(file_name)
     print('Всего: {0} файл(-а)'.format(len(files_str_out)))
 
-
+# Панель управления программой:
 while files_list is not None:
     print('\nНачинаем поиск')
     search_line = input('Введите условия поиска: ')  # Поисковый запрос
