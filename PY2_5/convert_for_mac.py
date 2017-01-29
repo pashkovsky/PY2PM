fe8c818d1f31ea7ec04fb130b4a1be3292ed1027
Mykola Pashkovskyi <mykola.pashkovsky@gmail.com>
2017-01-29 17:56:47 +0200

    Код в PY2_5/convert_for_mac.py выдает список изображений для конвертирования, но SIPS выдает error 4: not a valid file

--- PY2_5/convert_for_mac.py
+++ PY2_5/convert_for_mac.py
@@ -1,37 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import sys
 import os
-import glob
-
-width = '300'   # Требуемая ширина в пикселях
-
-# extension = ['jpg', 'JPG', 'JPEG','jpeg', 'png', 'gif', 'tiff', 'tif'] # Расширения\Форматы
-
-source = 'Source'
-out = 'Result'
-
-files = glob.glob(os.path.join(source, '*.jpg'))
-
-img_list = list(files)
-print(img_list)
 
 
-if not os.path.isdir(out):
-    os.mkdir(out)
+# Требуемая ширина в пикселях
+new_width = '200'
 
-print('\nТекущая директория %s \n' % (os.getcwd()))
-print('\nПапка с обработанными файлами %s \n' % (out))
-#
-# for elm in list_files:
-#     with open(elm) as file:
+# Создаем директорию, в которую будем помещать конвертированные изображения
+create_directory_out = subprocess.run(['mkdir', '-p', './Result'])
 
+# Директория, из которой будем брать изображения
+directory_source = 'Source'
+
+# Директория, в которую будем помещать конвертированные изображения
+directory_out = 'Result'
+
+# Получаем список файлов в переменную files
+files = os.listdir(directory_source)
+
+# Фильтруем список по маске  .jpg в переменную images
+# Решение взято с сайтов:
+# http://senkler.blogspot.com/2011/04/python.html
+# http://tutorialbox.freelancing.lv/tutorials/6
+images = filter(lambda x: x.endswith('.jpg'), files)
+
+img_list = list(images)
+print(img_list)  # Закомментировать
+
+# if not os.path.isdir(out):
+#     os.mkdir(out)
+
+# Отображаем текущую и целевую директории
+print('\nТекущая директория – %s' % (os.getcwd()))
+print('Директория с обработанными изображениями – %s \n' % directory_out)
+
+# Цикл конвертирования изображений с помощью программы sips
+for file_name in img_list:
+    print(file_name)  # Закомментировать
+    subprocess.run(['sips', '--resampleWidth', 'new_width', '--out', './directory_out/file_name',
+                    './directory_source/file_name', ])
+
+# TODO: Разобраться в чем причина ошибки "not a valid file"
+# Warning: ./directory_source/file_name not a valid file - skipping Error 4: no file was specified
 
 # Используйте команду sips
-#
 # Документация:
 # https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/sips.1.html
-#
 # Пример использования для нашей задачи:
 # sips --resampleWidth 200 myphoto.jpg
+# sips --resampleWidth 200 --out ./Result/face-04_200.jpg ./Source/face-04.jpg
